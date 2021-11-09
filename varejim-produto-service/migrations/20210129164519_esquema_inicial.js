b9a72018260ec72459b6e3149ca4b689a93fa0ec
Rafael Gomes de Magalhães <rafael.magalhaes@direcional.com.br>
2021-11-09 18:02:27 -0300

    Mudanças packages e migrations

--- varejim-produto-service/migrations/20210129164519_esquema_inicial.js
+++ varejim-produto-service/migrations/20210129164519_esquema_inicial.js
@@ -1,16 +1,16 @@
 exports.up = function (knex) {
-  return knex.schema
-    .createTable("secao", (tb) => {
-      tb.increments("id");
-      tb.string("descricao").notNullable();
-    })
-    .createTable("produto", (tb) => {
-      tb.increments("id");
-      tb.string("descricao").notNullable();
-      tb.integer("secao_id").references("secao.id");
-    });
-};
-
-exports.down = function (knex) {
-  return knex.schema.dropTable("produto").dropTable("secao");
-};
+    return knex.schema
+      .createTable("secao", (tb) => {
+        tb.increments("id");
+        tb.string("descricao").notNullable();
+      })
+      .createTable("produto", (tb) => {
+        tb.increments("id");
+        tb.string("descricao").notNullable();
+        tb.integer("secao_id").references("secao.id");
+      });
+  };
+  
+  exports.down = function (knex) {
+    return knex.schema.dropTable("produto").dropTable("secao");
+  };
--- varejim-produto-service/migrations/20210129190025_carga_inicial.js
+++ varejim-produto-service/migrations/20210129190025_carga_inicial.js
@@ -1,27 +1,27 @@
 exports.up = async function (knex) {
-  await knex("secao").insert([
-    { id: 100, descricao: "Bebidas" },
-    { id: 101, descricao: "Mercearia" },
-    { id: 102, descricao: "Doces" },
-    { id: 103, descricao: "Frios" },
-  ]);
-  return await knex("produto").insert([
-    { id: 1000, secao_id: 102, descricao: "Sorvete" },
-    { id: 1001, secao_id: 101, descricao: "Pimenta" },
-    { id: 1002, secao_id: 100, descricao: "Cerveja" },
-    { id: 1003, secao_id: 102, descricao: "Chocolate" },
-    { id: 1004, secao_id: 101, descricao: "Arroz" },
-    { id: 1005, secao_id: 101, descricao: "Feijão" },
-    { id: 1006, secao_id: 101, descricao: "Carne" },
-    { id: 1007, secao_id: 101, descricao: "Farofa" },
-    { id: 1008, secao_id: 101, descricao: "Queijo" },
-    { id: 1009, secao_id: 102, descricao: "Gelatina" },
-    { id: 1010, secao_id: 100, descricao: "Guaraná" },
-    { id: 1011, secao_id: 103, descricao: "Peixe" },
-  ]);
-};
-
-exports.down = async function (knex) {
-  await knex("produto").del();
-  return await knex("secao").del();
-};
+    await knex("secao").insert([
+      { id: 100, descricao: "Bebidas" },
+      { id: 101, descricao: "Mercearia" },
+      { id: 102, descricao: "Doces" },
+      { id: 103, descricao: "Frios" },
+    ]);
+    return await knex("produto").insert([
+      { id: 1000, secao_id: 102, descricao: "Sorvete" },
+      { id: 1001, secao_id: 101, descricao: "Pimenta" },
+      { id: 1002, secao_id: 100, descricao: "Cerveja" },
+      { id: 1003, secao_id: 102, descricao: "Chocolate" },
+      { id: 1004, secao_id: 101, descricao: "Arroz" },
+      { id: 1005, secao_id: 101, descricao: "Feijão" },
+      { id: 1006, secao_id: 101, descricao: "Carne" },
+      { id: 1007, secao_id: 101, descricao: "Farofa" },
+      { id: 1008, secao_id: 101, descricao: "Queijo" },
+      { id: 1009, secao_id: 102, descricao: "Gelatina" },
+      { id: 1010, secao_id: 100, descricao: "Guaraná" },
+      { id: 1011, secao_id: 103, descricao: "Peixe" },
+    ]);
+  };
+  
+  exports.down = async function (knex) {
+    await knex("produto").del();
+    return await knex("secao").del();
+  };
